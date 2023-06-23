a5c41b46c49a2c73d1b38f8ce9c5e79f98b21a2b
Phil Zona <phil.b.zona@gmail.com>
2023-06-23 16:27:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Dynamic Handlebars Templating with Node.js
+
+This project provides a script that allows you to dynamically render Handlebars templates using Node.js. It leverages the Handlebars templating engine, along with chokidar for file watching and node-sass for SCSS compilation.
+
+## Project Structure
+
+The project expects the following structure:
+
+- `src/`: This directory contains the source files.
+  - `index.hbs`: The main Handlebars template file.
+  - `partials/`: A directory containing partials to be included in the main template.
+  - `scss/`: A directory containing SCSS files.
+    - `style.scss`: The main SCSS file to be compiled.
+- `dist/`: This directory contains the output files.
+  - `css/`: A directory where the compiled CSS file will be generated.
+    - `style.css`: The compiled CSS file.
+- `src/`: This directory contains the context file.
+  - `context.js`: A JavaScript file that exports the context object to be used in the template.
+- `script.js`: The script file responsible for dynamically rendering the Handlebars template, watching for changes, and triggering recompilation.
+
+## Usage
+
+1. Install the dependencies by running `npm install`.
+2. Customize your Handlebars template in `src/index.hbs` and add any desired partials to `src/partials/`.
+3. Customize your SCSS styles in `src/scss/style.scss`.
+4. Customize your context object in `src/context.js`. Export the desired context object directly from this file.
+5. Run the script using `npm start`
+
+The script will watch for changes in the template, partials, SCSS files, and the context file. Whenever a change is detected, it will recompile the SCSS, update the CSS file, and render the Handlebars template with the updated context. The resulting HTML will be written to `dist/index.html`.
+
+Open `dist/index.html` in your browser to view the rendered template.
+
+Note: Ensure that the `dist/css/` directory exists before running the script. If it doesn't exist, the script will attempt to create it.
---
+++ index.js
@@ -0,0 +1,117 @@
+const fs = require('fs');
+const path = require('path');
+const handlebars = require('handlebars');
+const chokidar = require('chokidar');
+const sass = require('node-sass');
+
+const templatePath = path.join(__dirname, 'src', 'index.hbs');
+const partialsFolderPath = path.join(__dirname, 'src', 'partials');
+const scssFolderPath = path.join(__dirname, 'src', 'scss');
+const cssFolderPath = path.join(__dirname, 'dist', 'css');
+const distFolderPath = path.join(__dirname, 'dist');
+const distFilePath = path.join(distFolderPath, 'index.html');
+const styleFilePath = path.join(cssFolderPath, 'style.css');
+const contextFilePath = path.join(__dirname, 'src', 'context.js');
+
+// Read the initial content of the index.hbs file
+let templateContent = fs.readFileSync(templatePath, 'utf8');
+
+// Function to register all partials in the partials folder
+const registerPartials = () => {
+  // Clear all previously registered partials
+  handlebars.unregisterPartial(handlebars.partials);
+
+  // Read the files in the partials folder
+  const partialFiles = fs.readdirSync(partialsFolderPath);
+
+  // Register each partial
+  partialFiles.forEach((file) => {
+    const partialName = path.parse(file).name;
+    const partialPath = path.join(partialsFolderPath, file);
+    const partialContent = fs.readFileSync(partialPath, 'utf8');
+    handlebars.registerPartial(partialName, partialContent);
+  });
+};
+
+// Function to render the template with the given context object
+const renderTemplate = (context) => {
+  // Compile the template
+  const compiledTemplate = handlebars.compile(templateContent);
+
+  // Render the template with the context object
+  const renderedTemplate = compiledTemplate(context);
+
+  // Write the rendered template to index.html in the dist folder
+  fs.writeFileSync(distFilePath, renderedTemplate, 'utf8');
+
+  console.log('index.html updated');
+};
+
+// Watch for changes in the template, SCSS, and context files
+const watcher = chokidar.watch([templatePath, scssFolderPath, contextFilePath], {
+  ignored: /(^|[/\\])\../, // ignore dotfiles
+  persistent: true,
+});
+
+// When a change is detected, handle the corresponding file type
+watcher.on('change', (filePath) => {
+  const fileExt = path.extname(filePath);
+
+  if (fileExt === '.hbs') {
+    // Re-register the partials
+    registerPartials();
+
+    // Re-read the template content
+    templateContent = fs.readFileSync(templatePath, 'utf8');
+
+    // Read the context object from context.js
+    const context = require(contextFilePath);
+
+    // Render the template with the updated context object
+    renderTemplate(context);
+  } else if (fileExt === '.scss') {
+    // Compile SCSS to CSS
+    sass.render(
+      {
+        file: filePath,
+        outputStyle: 'compressed',
+      },
+      (err, result) => {
+        if (!err) {
+          // Write the compiled CSS to style.css in the CSS folder
+          fs.writeFileSync(styleFilePath, result.css, 'utf8');
+
+          console.log('style.css updated');
+        } else {
+          console.error('SCSS compilation error:', err);
+        }
+      }
+    );
+  } else if (filePath === contextFilePath) {
+    // Clear the require cache for context.js
+    delete require.cache[require.resolve(contextFilePath)];
+    
+    // Read the updated context object
+    const context = require(contextFilePath);
+
+    // Render the template with the updated context object
+    renderTemplate(context);
+  }
+});
+
+// Create the CSS folder if it doesn't exist
+if (!fs.existsSync(cssFolderPath)) {
+  fs.mkdirSync(cssFolderPath);
+}
+
+// Register the initial partials
+registerPartials();
+
+// Read the initial context object
+const initialContext = require(contextFilePath);
+
+// Render the template with the initial context object
+renderTemplate(initialContext);
+
+// Start watching for changes
+console.log('Watching for changes...');
